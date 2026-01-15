b93bdb497075d52c61739b2c2d94baa915954a13
Morikitan <morikitan25@gmail.com>
2026-01-15 18:22:11 +0900

    なんかエラー出てる

--- lib/servo/servo.cpp
+++ lib/servo/servo.cpp
@@ -21,10 +21,23 @@ void ServoSetup(){
     gpio_set_function(servo_right_claw_pin,GPIO_FUNC_PWM);
     gpio_set_function(servo_arm_left_and_right_pin,GPIO_FUNC_PWM);
     gpio_set_function(servo_arm_up_and_down_pin,GPIO_FUNC_PWM);
-
-
 }
 
-void SetArmAngle(int angle){
-
+//サーボの角度を設定するプログラム
+//pin : ピン番号
+//angle : 整数の角度(0°～180°(GPIO8,10は160°まで?))
+void SetServoAngle(unsigned int gpio,int angle){
+    if(0 <= angle && angle <= 180){
+        uint slice_num = pwm_gpio_to_slice_num(gpio);
+        pwm_set_enabled(slice_num,false);
+        if(gpio == 8 || gpio == 10){
+            //MG90S
+            // クロック分周器を100.0に設定 (150MHz / 100 = 1.5MHz)
+            pwm_set_clkdiv(slice_num, 100.0f);
+            // ラップ値を29,999に設定 (周期を50Hz = 20msにする) (1.5MHz / 30,000 = 50Hz)
+            pwm_set_wrap(slice_num, 29999);
+            //600 → 0° 1200 → 180°
+            pwm_set_gpio_level(gpio,(uint16_t)(600 + 600 * angle / 180));
+        }
+    }
 }
--- lib/servo/servo.hpp
+++ lib/servo/servo.hpp
@@ -5,6 +5,7 @@ extern "C" {
 #endif
 
 void ServoSetup();
+void SetServoAngle(unsigned int gpio,int angle);
 
 #ifdef __cplusplus
 }
