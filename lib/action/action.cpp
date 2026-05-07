66d89931c81d0d36146d559cc56281cd5a10ad24
pentyann32 <hisatopen@gmail.com>
2026-05-07 18:47:24 +0900

    コンパイルエラーの修正

--- lib/action/action.cpp
+++ lib/action/action.cpp
@@ -1377,21 +1377,16 @@ void TrashfromBasket(int object){
 }
 
 void TrashHorizonCan(){
-    RotationToAngle(270):
+    RotationToAngle(270);
     GetDataFromLineToMain();
     circleLineAngle = GetCircleLineVector(20,true,true);
     firstTime = time_us_32();
     //ゴールまで進む
-    while(!circleLineSensor[7] || !circleLineSensor[12] || (time_us_32() - firstTime) / 1000 < 750){
+    while(!frontLineSensor[0] || !frontLineSensor[2]){
         PrintDisplayMode();
         GetGyroAngleFromSub();
         GetDataFromLineToMain();
-        circleLineAngle = GetCircleLineVector(20,true,true);
-        if((time_us_32() - firstTime) / 2000 > 200){
-            MainMotorState(200 + (int)((angleX - 270) * 10),200 - (int)((angleX - 270) * 10));
-        }else{
-            MainMotorState((int)(time_us_32() - firstTime) / 2000 + (int)((angleX - 270) * 10),(int)(time_us_32() - firstTime) / 2000 - (int)((angleX - 270) * 10));
-        }
+        MainMotorState(200,200);
         SendBufferToDisplay();
     }
     MainMotorState(0,0);
@@ -1402,23 +1397,21 @@ void TrashHorizonCan(){
     sleep_ms(500);
     SetServoAngleFromMain(servo_left_claw_pin,160);
     SetServoAngleFromMain(servo_right_claw_pin,20);
+    sleep_ms(1000);
+    SetServoAngleFromMain(servo_arm_up_and_down_pin,90);
+    sleep_ms(1000);
     
     //線上に復帰
     firstTime = time_us_32();
-    while(VectorNumber != 4  || (time_us_32() - firstTime) / 1000 < 750){
+    while(!circleLineSensor[4] || !circleLineSensor[15]){
         PrintDisplayMode();
         GetGyroAngleFromSub();
         GetDataFromLineToMain();
-        circleLineAngle = GetCircleLineVector(20,true,true);
-        if((time_us_32() - firstTime) / 2000 > 200){
-            MainMotorState(-200 + (int)((angleX - 270) * 10),200 - (int)((angleX - 270) * 10));
-        }else{
-            MainMotorState((int)(time_us_32() - firstTime) / -2000 + (int)((angleX - 270) * 10),(int)(time_us_32() - firstTime) / -2000 - (int)((angleX - 270) * 10));
-        }
         SendBufferToDisplay();
+        MainMotorState(-200,-200);
     }
     MainMotorState(0,0);
-    RotationToAngle(180):
+    RotationToAngle(180);
 }
 
 //カメラ、ラインセンサ、ジャイロ、tof、カラーセンサ、電流センサを使う
